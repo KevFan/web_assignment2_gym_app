9df29e628ff0023bba2cf1c209fa67dfb02da89f
KevFan <kevin_fan@hotmail.co.uk>
2017-04-04 12:46:31 +0100

    Allow multiple assessment lists + added data.yml for use to describe initial values for model objects

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -5,6 +5,9 @@ import models.AssessmentList;
 import play.Logger;
 import play.mvc.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dashboard extends Controller
 {
   public static void index() {
@@ -13,10 +16,16 @@ public class Dashboard extends Controller
     Assessment a1 = new Assessment(60.2, 45.0, 12.2, 44.4, 38,38.0);
     Assessment a2 = new Assessment(56.6, 23.4, 24.5, 23.4, 45.2,22.4);
 
-    AssessmentList assessmentList = new AssessmentList("Assessment For Me");
-    assessmentList.assessments.add(a1);
-    assessmentList.assessments.add(a2);
+    AssessmentList al1 = new AssessmentList("Assessment For Me");
+    al1.assessments.add(a1);
 
-    render ("dashboard.html", assessmentList);
+    AssessmentList al2 = new AssessmentList("Assessment For You");
+    al2.assessments.add(a2);
+
+    List<AssessmentList> assessmentLists = new ArrayList<AssessmentList>();
+    assessmentLists.add(al1);
+    assessmentLists.add(al2);
+
+    render ("dashboard.html", assessmentLists);
   }
 }
