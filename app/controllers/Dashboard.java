37c83046f4826177b5e53c56a279602846a73efc
KevFan <kevin_fan@hotmail.co.uk>
2017-04-09 19:06:56 +0100

    Added determine bmi category + changes in views to show bmi category

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -61,4 +61,42 @@ public class Dashboard extends Controller
         return (int)(num *100 ) / 100.0;
     }
 
+
+    /**
+     * This method determines the BMI category that the member belongs to.
+     *
+     * <pre>
+     *
+     * The category is determined by the magnitude of the members BMI according to the following:
+     *
+     *     BMI less than    15   (exclusive)                      is "VERY SEVERELY UNDERWEIGHT"
+     *     BMI between      15   (inclusive) and 16   (exclusive) is "SEVERELY UNDERWEIGHT"
+     *     BMI between      16   (inclusive) and 18.5 (exclusive) is "UNDERWEIGHT"
+     *     BMI between      18.5 (inclusive) and 25   (exclusive) is "NORMAL"
+     *     BMI between      25   (inclusive) and 30   (exclusive) is "OVERWEIGHT"
+     *     BMI between      30   (inclusive) and 35   (exclusive) is "MODERATELY OBESE"
+     *     BMI between      35   (inclusive) and 40   (exclusive) is "SEVERELY OBESE"
+     *     BMI greater than 40   (inclusive)                      is "VERY SEVERELY OBESE"
+     *
+     * </pre>
+     *
+     * @return
+     * <pre>
+     * The format of the String is similar to this (note the double quotes around the category):
+     *     "NORMAL".
+     * </pre>
+     */
+    public static String determineBMICategory()
+    {
+        double bmi = calculateBMI();
+
+        if      (bmi < 15)    return "VERY SEVERELY UNDERWEIGHT";
+        else if (bmi < 16)    return "SEVERELY UNDERWEIGHT";
+        else if (bmi < 18.5)  return "UNDERWEIGHT";
+        else if (bmi < 25)    return "NORMAL";
+        else if (bmi < 30)    return "OVERWEIGHT";
+        else if (bmi < 35)    return "MODERATELY OBESE";
+        else if (bmi < 40)    return "SEVERELY OBESE";
+        else                  return "VERY SEVERELY OBESE";
+    }
 }
