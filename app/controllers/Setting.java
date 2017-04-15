7ca81f7559552424ce2a12e1516f5265d39fb150
KevFan <kevin_fan@hotmail.co.uk>
2017-04-15 20:31:14 +0100

    Added update options to member account details

--- app/controllers/Setting.java
+++ app/controllers/Setting.java
@@ -13,4 +13,53 @@ public class Setting extends Controller {
         Member member = Accounts.getLoggedInMember();
         render("setting.html", member);
     }
+
+    public static void updateMemberName(String name) {
+        Member member = Accounts.getLoggedInMember();
+        member.name = name;
+        member.save();
+        redirect("/setting");
+    }
+
+    public static void updateMemberGender(String gender) {
+        Member member = Accounts.getLoggedInMember();
+        member.gender = gender;
+        member.save();
+        redirect("/setting");
+    }
+
+    public static void updateMemberEmail(String email) {
+        Member member = Accounts.getLoggedInMember();
+        member.email = email;
+        member.save();
+        redirect("/setting");
+    }
+
+    public static void updateMemberPassword(String password) {
+        Member member = Accounts.getLoggedInMember();
+        member.password = password;
+        member.save();
+        redirect("/setting");
+    }
+
+    public static void updateMemberAddress(String address) {
+        Member member = Accounts.getLoggedInMember();
+        member.address = address;
+        member.save();
+        redirect("/setting");
+    }
+
+    public static void updateMemberHeight(double height) {
+        Member member = Accounts.getLoggedInMember();
+        member.height = height;
+        member.save();
+        redirect("/setting");
+    }
+
+    public static void updateMemberStartingWeight(double startingWeight) {
+        Member member = Accounts.getLoggedInMember();
+        member.startingWeight = startingWeight;
+        member.save();
+        redirect("/setting");
+    }
 }
