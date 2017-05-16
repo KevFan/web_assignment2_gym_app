fa8503c922e14e53e6584b4373e7ea4923af93ff
KevFan <kevin_fan@hotmail.co.uk>
2017-05-16 16:08:04 +0100

    Image for login, register and favicon for

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,17 +1,17 @@
-import java.util.List;
-
-        import play.*;
-        import play.jobs.*;
-        import play.test.*;
-
-        import models.*;
+import play.jobs.*;
+import play.test.*;
+import models.*;
 
 /**
+ * Class that loads the data yml file to the play database
  * Created by kevin on 04/04/2017.
  */
 
 @OnApplicationStart
 public class Bootstrap extends Job {
+    /**
+     * Method that loads the data.yml file when there are no members in the database
+     */
     public void doJob() {
         if(Member.count() == 0) {
             Fixtures.loadModels("data.yml");
